156e677be546b15607c40f64d6f31fe10ebbc13b
Joshua Gomez <joshua.gomez@quest-global.com>
2018-07-02 09:56:16 +0530

    Initial commit

---
+++ commonlib/src/main/java/com/josh/gomez/commonlib/logutils/TraceLog.java
@@ -0,0 +1,23 @@
+package com.josh.gomez.commonlib.logutils;
+
+import android.util.Log;
+
+public class TraceLog {
+
+    public static void entryLog() {
+        StackTraceElement element = Thread.currentThread().getStackTrace()[3];
+        String className = element.getClassName();
+        className = className.substring(className.lastIndexOf(".") + 1, className.length());
+        String methodName = element.getMethodName();
+        Log.i(className, methodName + " >>> Entry");
+    }
+
+    public static void exitLog() {
+        StackTraceElement element = Thread.currentThread().getStackTrace()[3];
+        String className = element.getClassName();
+        className = className.substring(className.lastIndexOf(".") + 1, className.length());
+        String methodName = element.getMethodName();
+        Log.i(className, methodName + " <<< Exit");
+    }
+
+}
